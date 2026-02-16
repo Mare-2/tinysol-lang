ba67a0f5eef569f37c18b9c29309f553800092fa
Ghirons <fghironi59@gmail.com>
2026-02-17 00:36:06 +0100

    Add initial contract and unit tests for view semantics (mapping)

--- contracts/ViewSemantic/MapW.sol
+++ contracts/ViewSemantic/MapW.sol
@@ -1,2 +1,16 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity >= 0.8.2;
+
+contract C {
+    mapping (int => uint) public Prova;
+    uint public x;
+
+    function g() public { // function non view e assegnamento con mapping
+        Prova[0]=1;
+        x = Prova[0];
+    }
+
+    function f() public view { // function view e fallimento 
+        Prova[0] = 1;
+    }
+}
